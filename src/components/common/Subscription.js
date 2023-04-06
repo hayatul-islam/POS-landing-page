10551fdd4c2303a8cf86c50c650db73664ab829b
hayatul-islam <hayatulemon340@gmail.com>
2023-04-06 14:54:16 +0600

    subcription design

---
+++ src/components/common/Subscription.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Subscription = () => {
+  return (
+    <div className="bg-[#2d7de9] px-6">
+      <div className="text-center py-8 md:py-12 text-white space-y-10 max-w-2xl mx-auto">
+        <div className="space-y-2">
+          <h2 className="text-[24px] md:text-[28px]">
+            Square POS is a great option for businesses
+          </h2>
+          <p className="text-[16px] md:text-[18px]">
+            A business uses a POS system to process card payments or other forms
+            of electronic payments at a physical location
+          </p>
+        </div>
+        <button className="py-2 w-[200px] text-[14px] border border-white rounded-full text-white">
+          Subscription
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Subscription;
--- src/pages/TheLayout.js
+++ src/pages/TheLayout.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "../components/common/Header";
-import Home from "./Home";
+import Subscription from "../components/common/Subscription";
 
 const TheLayout = () => {
   return (
     <div>
-      <Header />
-      <Home />
+      {/* <Header />
+      <Home /> */}
+      <Subscription />
     </div>
   );
 };
